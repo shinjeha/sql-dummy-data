733eadda9080821075005342d9449e72f27f9d6b
신 <67593107+shinjeha@users.noreply.github.com>
2025-06-20 16:50:46 +0900

    Update ecommerce_dummy_data.sql

--- ecommerce_dummy_data.sql
+++ ecommerce_dummy_data.sql
@@ -5,6 +5,10 @@
 -- 아니면
 -- mysql -u your_user -p your_database < ecommerce_dummy_data.sql
 
+-- Database Creation
+DROP DATABASE IF EXISTS ecommerce_db;
+CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
 
 -- Schema Creation
 CREATE TABLE users (
